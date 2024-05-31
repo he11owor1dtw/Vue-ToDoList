e44df8159b55836f57aab05aedb34db2c1d5011c
HelloCodeman <955khm@gmail.com>
2024-05-31 17:19:56 +0800

    fetch 先定義好的todo列表，並運用生命週期函數onMounted掛載

--- src/composables/useTodos.js
+++ src/composables/useTodos.js
@@ -1,11 +1,32 @@
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 export default function useTodos() {
   const todos = ref([]);
+  // 增加 todo
   const addTodo = (todo) => todos.value.push(todo);
+
+  // fetch 自製 json 檔當作既有列表
+  const fetchTodos = async () => {
+    const response = await fetch("./todos.json");
+    const rawTodos = await response.json();
+
+    // 使用了 JavaScript 的 map 方法，將 rawTodos 數組中的每個元素映射為新的數組
+    // 對於每個 todo 對象，我們都要執行一段箭頭函數來轉換它的格式，它定義了如何轉換每個 todo 對象的格式
+    todos.value = rawTodos.map((todo) => ({
+      id: todo.id,
+      content: todo.title,
+      completed: todo.completed,
+    }));
+  };
+
+  // 在組件掛載後調用 fetchTodos 函數以加載數據
+  onMounted(() => {
+    fetchTodos();
+  });
 
   return {
     todos,
     addTodo,
   };
+
 }
